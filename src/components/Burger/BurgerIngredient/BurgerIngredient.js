b55f596dd0e719d6291ea93f11f2fa3f947d7754
Jake Thomas <jmaxt12@gmail.com>
2019-12-18 19:47:05 -0600

    now can add and remove ingredients.

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -7,11 +7,11 @@ class BurgerIngredient extends Component {
     render () {
         let ingredient = null;
 
-        switch(this.props.types) {
-            case('bread-bottom'):
+        switch ( this.props.type ) {
+            case ( 'bread-bottom' ):
                 ingredient = <div className={classes.BreadBottom}></div>;
                 break;
-            case('bread-top'):
+            case ( 'bread-top' ):
                 ingredient = (
                     <div className={classes.BreadTop}>
                         <div className={classes.Seeds1}></div>
@@ -19,28 +19,28 @@ class BurgerIngredient extends Component {
                     </div>
                 );
                 break;
-            case('meat'):
+            case ( 'meat' ):
                 ingredient = <div className={classes.Meat}></div>;
                 break;
-            case('cheese'):
+            case ( 'cheese' ):
                 ingredient = <div className={classes.Cheese}></div>;
                 break;
-            case('salad'):
-                ingredient = <div className={classes.Salad}></div>;
-                break;
-            case('bacon'):
+            case ( 'bacon' ):
                 ingredient = <div className={classes.Bacon}></div>;
+                break;
+            case ( 'salad' ):
+                ingredient = <div className={classes.Salad}></div>;
                 break;
             default:
                 ingredient = null;
-        };
+        }
 
         return ingredient;
     }
-} 
+}
 
 BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
 };
- 
+
 export default BurgerIngredient;
