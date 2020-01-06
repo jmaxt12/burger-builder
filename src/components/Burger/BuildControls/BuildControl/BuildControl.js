be69132d1492de6583a4469540adebdc83c1f3e7
Jake Thomas <jmaxt12@gmail.com>
2020-01-06 13:48:31 -0700

    added buttons for control

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BuildControl = (props) => (
+    <div>
+        <div>{props.label}</div>
+        <button>Less</button>
+        <button>More</button>
+    </div>
+)
+ 
+export default BuildControl;
