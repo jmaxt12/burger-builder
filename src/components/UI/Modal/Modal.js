4b104da48c813fe96214105f326d3fc6b9b1c9c0
Jake Thomas <jmaxt12@gmail.com>
2020-01-08 14:19:11 -0700

    fixed modal error. *typo

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,20 +4,20 @@ import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
+const modal = ( props ) => (
     <Aux>
         <Backdrop 
-        show={props.show}
-        clicked={props.modalClosed}/>
-        <div 
+            show={props.show} 
+            clicked={props.modalClosed} />
+        <div
             className={classes.Modal}
             style={{
-                transform: props.show ? 'translateY(0)' : 'translateT(-100vh)',
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
                 opacity: props.show ? '1' : '0'
             }}>
             {props.children}
         </div>
     </Aux>
 );
- 
+
 export default modal;
