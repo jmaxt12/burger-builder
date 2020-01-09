ebe48a7f41dac121d9377bcf771ed42c53a4704d
Jake Thomas <jmaxt12@gmail.com>
2020-01-09 12:37:25 -0700

    added more side drawer responsiveness

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={Classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)   
- 
-export default layout;
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+    
+
+    render () {
+        return (
+            <Aux>
+            <Toolbar />
+            <SideDrawer 
+                open={this.state.showSideDrawer} 
+                closed={this.sideDrawerClosedHandler}/>
+            <main className={Classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+        )
+    }
+}    
+  
+
+export default Layout;
