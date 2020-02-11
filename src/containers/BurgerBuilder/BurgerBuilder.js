6dda89a99502e3596914bec7b2b40d2393e71790
Jake <jmaxt12@gmail.com>
2020-02-11 14:34:24 -0700

    added error handling globaly

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -101,13 +102,13 @@ class BurgerBuilder extends Component {
             },
             deliveryMethod: 'fastest'
         }
-        // axios.post( '/orders.json', order )
-        //     .then( response => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     } )
-        //     .catch( error => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     } );
+        axios.post( '/orders.json', order )
+            .then( response => {
+                this.setState({ loading: false, purchasing: false });
+            } )
+            .catch( error => {
+                this.setState({ loading: false, purchasing: false });
+            } );
     }
 
     render () {
@@ -144,4 +145,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
